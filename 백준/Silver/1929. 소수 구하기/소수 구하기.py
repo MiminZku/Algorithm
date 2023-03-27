c1762508b5a57cb53b1804baac0af275f0fe7873
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-03-28 01:45:21 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 41052 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,10 +2,11 @@ import sys; input = lambda : sys.stdin.readline().rstrip()
 import math
 
 n, m = map(int,input().split())
-arr = [True for _ in range(m+1)]
+arr = [True] * (m+1)
 arr[1] = False
 
-for i in range(2,int(math.sqrt(m))+1):
+end = int(math.sqrt(m))
+for i in range(2,end+1):
     if arr[i] == False:  continue
     for j in range(2*i, m+1, i):
         arr[j] = False
