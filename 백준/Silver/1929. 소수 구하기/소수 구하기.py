2ec291d008d3099fc39eb418dfb70d9b3df59442
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-03-28 01:36:04 +0900

    [Silver III] Title: 소수 구하기, Time: 672 ms, Memory: 41628 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,17 @@
 import sys; input = lambda : sys.stdin.readline().rstrip()
-
-def is_prime(n):
-    if n == 1:  return False
-    if n>2 and n % 2 == 0:  return False
-    for i in range(3,int(n**(0.5))+1,2):
-        if n % i == 0:
-            return False
-    return True
+import math
 
 n, m = map(int,input().split())
-for i in range(n, m+1):
-    if is_prime(i):
+arr = [True for _ in range(m+1)]
+arr[1] = False
+
+for i in range(2,int(math.sqrt(m))+1):
+    if arr[i] == False:  continue
+    j = 2
+    while i*j <= m:
+        arr[i*j] = False
+        j += 1
+            
+for i in range(n,m+1):
+    if arr[i] == True:
         print(i)
