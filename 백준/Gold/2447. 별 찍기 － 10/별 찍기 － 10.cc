51996a50ee0545e40ef59b7348fec52304f8c8bf
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-04-20 15:19:49 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 172 ms, Memory: 6696 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+char arr[2188][2188];
+
+void star(int rS, int rE, int cS, int cE){
+    if(rS == rE){
+        arr[rS][cS] = '*';
+        return;
+    }
+    int len = (rE-rS+1)/3;
+    for (int r = 0; r < 3; r++){
+        for(int c = 0; c < 3; c++){
+            if(r==1 && c==1)    continue;
+            star(rS+r*len, rS+(r+1)*len-1, cS+c*len, cS+(c+1)*len-1);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            arr[i][j] = ' ';
+        }
+    }
+    
+    star(1, n, 1, n);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout<<arr[i][j];
+        }
+        cout<<'\n';
+    }
+}
