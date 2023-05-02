b8d7a6c6d67802385f992613986c3a86486742f4
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-05-02 23:20:14 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,19 +1,20 @@
-#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
 int main(){
+    cin.tie(nullptr); ios::sync_with_stdio(false);
     int n,m;
-    scanf("%d %d",&n,&m);
+    cin>>n>>m;
     int arr[n+1] ={0};
     for(int i=1; i<=n; i++){
         int k;
-        scanf("%d",&k);
+        cin>>k;
         arr[i] = arr[i-1]+k;
     }              
     for(int i=0; i<m; i++){
         int a,b;
-        scanf("%d %d",&a,&b);
-        printf("%d\n",arr[b]-arr[a-1]);
+        cin>>a>>b;
+        cout<<arr[b]-arr[a-1]<<'\n';
     }
 }
