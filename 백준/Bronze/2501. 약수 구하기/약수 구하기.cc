4f907a89213af5cd63e2e03db57ff94d53f301c8
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-05-27 18:01:33 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);   cin.tie(NULL);
+    int n,k;
+    cin>>n>>k;
+    int count = 0;
+    for(int i=1; i<=n; i++){
+        if(n%i==0)  count++;
+        if(count==k){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+    cout<<0<<endl;
+}
