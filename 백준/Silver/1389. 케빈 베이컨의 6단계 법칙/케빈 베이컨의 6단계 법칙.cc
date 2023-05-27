9dff2865d027eef41ba400da06901780c2a9d120
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-05-27 16:16:42 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int n,m;
+set<int> navers[101];
+int dist[101][101];
+
+
+void BFS(int start){
+    bool visited[101] = {};
+    visited[start] = true;
+    queue<pair<int,int>> q;
+    q.push({0,start});
+
+    while(!q.empty()){
+        int d = q.front().first;
+        int cur = q.front().second;
+        q.pop();
+        dist[start][cur] = d;
+        for(int naver: navers[cur]){
+            if(visited[naver])  continue;
+            visited[naver] = true;
+            q.push({d+1, naver});
+        }
+    }
+}
+
+int main(){
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    cin>>n>>m;
+
+    for(int i=0; i<m; i++){
+        int a,b;
+        cin>>a>>b;
+        navers[a].insert(b);
+        navers[b].insert(a);
+    }
+
+    int min = 10000; int res;
+    for(int i=1; i<=n; i++){
+        BFS(i);
+        int sum = 0;
+        for(int j=1; j<=n; j++){
+            sum+=dist[i][j];
+        }
+        if(min>sum){
+            min = sum;
+            res = i;
+        };
+    }
+    cout<<res<<endl;
+}
