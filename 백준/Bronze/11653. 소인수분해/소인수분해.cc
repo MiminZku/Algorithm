d2123a67e33d326448353325d46d846a7355a618
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-05-27 18:54:50 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,33 +1,19 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int n;
-bool IsNotPrime[10000001];
-vector<int> primeNums;
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(NULL);
     cin>>n;
     if(n==1)    return 0;
-    IsNotPrime[1] = true;
-    for(int i=2; i*i<=10000000; i++){
-        for(int j=2; i*j<=10000000; j++){
-            if(IsNotPrime[i*j])    continue;
-            IsNotPrime[i*j] = true;
-        }
-    }
-    for(int i=1; i<=10000000; i++){
-        if(!IsNotPrime[i])  primeNums.push_back(i);
-    }
-    
-    int idx=0;
+    int primeNum = 2;
     while(n!=1){
-        if(n % primeNums[idx])  idx++;
+        if(n%primeNum)  primeNum++;
         else{
-            cout<<primeNums[idx]<<endl;
-            n /= primeNums[idx];
+            cout<<primeNum<<endl;
+            n /= primeNum;
         }
     }
 }
