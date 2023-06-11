78366f2e200ef43e57a96aa66c9e873e5ce77e6f
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-06-11 15:22:32 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n,m;
+int nums[8];
+int path[8];
+bool picked[8];
+
+void Recur(int level){
+    if(level == m){
+        for(int i: path){
+            if(i)   cout<<i<<' ';
+            else{
+                cout<<'\n';
+                break;
+            }
+        }
+        return;
+    }
+    for(int i=0; i<n; i++){
+        if(picked[i])   continue;
+        picked[i] = true;
+        path[level] = nums[i];
+        Recur(level+1);
+        picked[i] = false;
+    }
+}
+
+int main(){
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    cin>>n>>m;
+    for(int i=0; i<n; i++)  cin>>nums[i];
+    sort(nums, nums+n);
+    Recur(0);
+}
