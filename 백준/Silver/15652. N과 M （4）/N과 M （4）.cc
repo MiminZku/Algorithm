22a4be6067153ce373afd92d6961c02d89bcc9c5
Mingi Kang <82875083+MiminZku@users.noreply.github.com>
2023-08-28 14:26:33 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -5,24 +5,31 @@ using namespace std;
 int n,m;
 int path[8];
 
-void Recur(int level, int start){
-    if(level == m){
-        for(int i: path){
+void Recur(int level, int start)
+{
+    if(level == m)
+    {
+        for(int i: path)
+        {
             if(i)   cout<<i<<' ';
-            else{
+            else
+            {
                 cout<<'\n';
                 break;
             }
         }
         return;
     }
-    for(int i=start; i<=n; i++){
+    for(int i=start; i<=n; i++)
+    {
         path[level] = i;
         Recur(level+1, i);
     }
 }
 
-int main(){
+int main()
+{
+    cin.tie(nullptr); ios::sync_with_stdio(false);
     cin>>n>>m;
     Recur(0, 1);
 }
