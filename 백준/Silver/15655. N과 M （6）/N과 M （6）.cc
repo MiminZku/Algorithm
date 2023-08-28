a8a1d9d32525a71a39b166267b526cecbad2ccf0
Mingi Kang <82875083+MiminZku@users.noreply.github.com>
2023-08-28 14:31:02 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n,m;
+int nums[8];
+int path[8];
+bool visited[8];
+
+void Recur(int level, int start)
+{
+    if(level == m)
+    {
+        for(int i: path)
+        {
+            if(i)   cout<<i<<' ';
+            else
+            {
+                cout<<'\n';
+                break;
+            }
+        }
+        return;
+    }
+    for(int i=start; i<n; i++)
+    {
+        if(visited[i])   continue;
+        visited[i] = true;
+        path[level] = nums[i];
+        Recur(level+1, i+1);
+        visited[i] = false;
+    }
+}
+
+int main()
+{
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    cin>>n>>m;
+    for(int i=0; i<n; i++)  cin>>nums[i];
+    sort(nums, nums+n);
+    Recur(0, 0);
+}
