84ef85e2c10bcc13d05ebc0b563ca6157be3bc64
Mingi Kang <82875083+MiminZku@users.noreply.github.com>
2023-11-09 18:04:16 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const double PI = 3.141592653589793;
+    double r;
+    cin>>r;
+    printf("%.6lf\n", PI*r*r);
+    printf("%.6lf\n", 2*r*r);
+}
