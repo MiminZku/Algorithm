dfe3e1fafeb198d65a61b1041bd6ef787c0c70fd
Mingi Kang <rkdalsrl111@naver.com>
2023-12-09 20:49:03 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    for(int i=0; i<str.length(); i++)
+    {
+        if(i!=0 && i%10==0) cout<<"\n";
+        cout<<str[i];
+    }
+    cout<<endl;
+}
