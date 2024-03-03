8cbd53489088ab09b1d9cd2d63158c2632040230
Mingi Kang <rkdalsrl111@naver.com>
2024-03-03 14:21:49 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(ch == 'n' || ch == 'N')  cout<<"Naver D2\n";
+    else                        cout<<"Naver Whale\n";
+}
