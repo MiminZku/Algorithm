7f987f060b078a003c088280eaeb1c5ba6c91ae5
Mingi Kang <rkdalsrl111@naver.com>
2024-04-05 16:21:57 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+
+    int h,i,a,r,c;
+    cin>>h>>i>>a>>r>>c;
+
+    cout<<h*i-a*r*c<<endl;
+}
