339d9e903df5e942d3f7a9bd6ea4102f5176024b
Mingi Kang <rkdalsrl111@naver.com>
2024-07-03 11:29:22 +0900

    [Silver II] Title: 차이를 최대로, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
@@ -3,41 +3,44 @@
 using namespace std;
 
 int n;
+int arr[8];
 int path[8];
-bool visited[10];
-int arr[10];
+int visited[8];
 int res;
 
 void Recur(int level)
 {
-    if(level == n)
-    {
-        int sum = 0;
-        for(int i=1; i<n; i++)
-        {
-            sum += (path[i]>path[i-1]) ? (path[i]-path[i-1]) : (path[i-1]-path[i]);
-        }
-        if(sum > res)   res = sum;
-        return;
-    }
-    for(int i=0; i<n; i++)
-    {
-        if(visited[i])  continue;
-        visited[i] = true;
-        path[level] = arr[i];
-        Recur(level+1);
-        visited[i] = false;
-    }
+	if (level == n)
+	{
+		int sum = 0;
+		for (int i = 1; i < n; ++i)
+		{
+			sum += abs(path[i] - path[i - 1]);
+		}
+
+		res = max(res, sum);
+
+		return;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (visited[i])	continue;
+		visited[i] = true;
+		path[level] = arr[i];
+		Recur(level + 1);
+		visited[i] = false;
+	}
 }
 
 int main()
 {
-    cin.tie(nullptr); ios::sync_with_stdio(false);
-    cin>>n;
-    for(int i=0; i<n; i++)
-    {
-        cin>>arr[i];
-    }
-    Recur(0);
-    cout<<res<<endl;
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+
+	Recur(0);
+
+	cout << res << endl;
 }
