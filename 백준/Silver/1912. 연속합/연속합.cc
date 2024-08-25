12ad9e6dbf5b8f4e13f91f3a4340ad59c0eebe59
Mingi Kang <rkdalsrl111@naver.com>
2024-08-25 22:56:10 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int n;
-vector<int> arr;
-vector<int> memo[2];
+int preSumMax[101010];
+int ans = -2147483648;
 
 int main()
 {
     cin.tie(nullptr); ios::sync_with_stdio(false);
-    cin>>n;
-    arr.resize(n);
-    memo[0].resize(n);
-    memo[1].resize(n);
-    for(int i=0; i<n; i++)
+
+    cin >> n;
+
+    for (int i = 1, num; i <= n; ++i)
     {
-        cin>>arr[i];
+        cin >> num;
+        preSumMax[i] = max(num, preSumMax[i - 1] + num);
+        ans = max(ans, preSumMax[i]);
     }
 
-    memo[0][0] = arr[0];
-    memo[1][0] = arr[0];
-    for(int i=1; i<n; i++)
-    {
-        memo[0][i] = max(memo[0][i-1], memo[1][i-1]);
-        memo[1][i] = max(arr[i], memo[1][i-1] + arr[i]);
-    }
-
-    int result = max(memo[0][n-1], memo[1][n-1]);
-    cout<<result<<endl;
+    cout << ans << endl;
 }
