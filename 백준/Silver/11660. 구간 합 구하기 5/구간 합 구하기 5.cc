658c15e44da41981e664824e465ab8227dce9824
Mingi Kang <rkdalsrl111@naver.com>
2024-08-25 21:00:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,38 +2,36 @@
 
 using namespace std;
 
-int n,m;
+int n, m;
+int mat[1111][1111];
+int cumulativeSum[1111][1111];
 
-int main(){
-    cin.tie(nullptr); ios::sync_with_stdio(false);
-    cin>>n>>m;
-    long long mat[n][n];
-    
-    cin>>mat[0][0];
-    for(int j=1; j<n; j++){
-        cin>>mat[0][j];
-        mat[0][j] += mat[0][j-1];
-    }
-    for(int i=1; i<n; i++){
-        cin>>mat[i][0];
-        mat[i][0] += mat[i-1][0];
-        for(int j=1; j<n; j++){
-            cin>>mat[i][j];
-            mat[i][j] += mat[i][j-1] + mat[i-1][j] - mat[i-1][j-1];
-        }
-    }
+int GetSubMatrixSum(int r1, int c1, int r2, int c2)
+{
+	return cumulativeSum[r2][c2]
+		- cumulativeSum[r2][c1 - 1]
+		- cumulativeSum[r1 - 1][c2]
+		+ cumulativeSum[r1 - 1][c1 - 1];
+}
 
-    while(m--){
-        int r1,c1,r2,c2;
-        cin>>r1>>c1>>r2>>c2;
-        r1--;   c1--;
-        r2--;   c2--;
-        
-        long long result = mat[r2][c2];
-        if(r1)  result -= mat[r1-1][c2];
-        if(c1)  result -= mat[r2][c1-1];
-        if(r1 && c1)    result += mat[r1-1][c1-1];
-        
-        cout<<result<<'\n';
-    }
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+		{
+			cin >> mat[i][j];
+			cumulativeSum[i][j] +=
+				cumulativeSum[i - 1][j] + cumulativeSum[i][j - 1] + mat[i][j] - cumulativeSum[i - 1][j - 1];
+		}
+	}
+
+	while (m--)
+	{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << GetSubMatrixSum(x1, y1, x2, y2) << '\n';
+	}
 }
