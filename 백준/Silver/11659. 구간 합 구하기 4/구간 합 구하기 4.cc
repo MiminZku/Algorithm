22bdc28768a892103a1983970b2f17f67728aa23
Mingi Kang <rkdalsrl111@naver.com>
2024-08-25 18:57:38 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -2,19 +2,25 @@
 
 using namespace std;
 
-int main(){
-    cin.tie(nullptr); ios::sync_with_stdio(false);
-    int n,m;
-    cin>>n>>m;
-    int arr[n+1] ={0};
-    for(int i=1; i<=n; i++){
-        int k;
-        cin>>k;
-        arr[i] = arr[i-1]+k;
-    }              
-    for(int i=0; i<m; i++){
-        int a,b;
-        cin>>a>>b;
-        cout<<arr[b]-arr[a-1]<<'\n';
-    }
+int n, m;
+int cumSum[100001];
+
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)
+	{
+		int k;
+		cin >> k;
+		cumSum[i] = cumSum[i - 1] + k;
+	}
+
+	while (m--)
+	{
+		int i, j;
+		cin >> i >> j;
+		cout << cumSum[j] - cumSum[i - 1] << '\n';
+	}
 }
