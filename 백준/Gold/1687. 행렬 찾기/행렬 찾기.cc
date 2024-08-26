0ced66836672786e747c34ee6d39348c151ac0b7
Mingi Kang <rkdalsrl111@naver.com>
2024-08-27 07:07:11 +0900

    [Gold III] Title: 행렬 찾기, Time: 36 ms, Memory: 2896 KB -BaekjoonHub

--- 백준/Gold/1687. 행렬 찾기/행렬 찾기.cc
+++ 백준/Gold/1687. 행렬 찾기/행렬 찾기.cc
@@ -1,55 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <string>
- 
+
 using namespace std;
- 
+
+int n, m;
+int mat[335][335];
+int cumulativeSum[335][335];
+
+int GetSubMatrixSum(int r1, int c1, int r2, int c2)
+{
+	return cumulativeSum[r2][c2]
+		- cumulativeSum[r2][c1 - 1]
+		- cumulativeSum[r1 - 1][c2]
+		+ cumulativeSum[r1 - 1][c1 - 1];
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
- 
-    int n, m;
-    cin >> n >> m;
- 
-    vector<vector<int>> vec(n + 1, vector<int>(m + 1, 0));
-    vector<vector<int>> prefix(n + 1, vector<int>(m + 1, 0));
- 
-    string temp;
- 
-    for (int i = 1; i < n + 1; i++)
-    {
-        cin >> temp;
-        for (int j = 1; j < m + 1; j++)
-        {
-            if (temp[j - 1] == '0') vec[i][j] = 1;
-            else if (temp[j - 1] == '1') vec[i][j] = 0;
-            prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + vec[i][j];
-        }
-    } 
- 
-    int ans = 0;
-    // 열 2개 선택
-    for (int i = 1; i < m + 1; i++)
-    {
-        for (int j = i; j < m + 1; j++)
-        {
-            // 행을 따라 내려가면서
-            int cnt = 0;
-            for (int k = 1; k < n + 1; k++)
-            {
-                int temp = prefix[k][j] - prefix[k][i - 1] - prefix[k - 1][j] + prefix[k - 1][i - 1];
-                if (temp == j - i + 1)
-                {
-                    cnt += temp;
-                    ans = max(ans, cnt);
-                }
-                else
-                {
-                    cnt = 0;
-                }
-            }
-        }
-    } 
-    cout << ans << endl;
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	cin >> n >> m;
+	cin.ignore();
+	char ch;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= m; ++j)
+		{
+			cin >> ch;
+			mat[i][j] = ch - '0' ? 0 : 1;
+			cumulativeSum[i][j] =
+				mat[i][j] + cumulativeSum[i - 1][j] + cumulativeSum[i][j - 1] - cumulativeSum[i - 1][j - 1];
+		}
+	}
+
+	int ans = 0;
+	// 열 2개 선택
+	for (int i = 1; i <= m; ++i)
+	{
+		for (int j = i; j <= m; ++j)
+		{
+			// 행을 따라 내려가면서
+			int cnt = 0;
+			for (int k = 1; k <= n; ++k)
+			{
+				int sum = GetSubMatrixSum(k, i, k, j);
+				if (j - i + 1 == sum)
+				{
+					cnt += sum;
+					ans = max(ans, cnt);
+				}
+				else
+				{
+					cnt = 0;
+				}
+			}
+		}
+	}
+	cout << ans << endl;
 }
