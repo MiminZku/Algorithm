bf1166b1d6ad0124523da44eb7d2851799c260f8
Mingi Kang <rkdalsrl111@naver.com>
2024-09-29 01:17:22 +0900

    [Gold III] Title: 파일 합치기, Time: 160 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -1,47 +1,54 @@
 #include <iostream>
 
-#define fastio cin.tie(nullptr), ios::sync_with_stdio(false);
-#define INF 2147483647;
-
 using namespace std;
 
-int fileSize[501][501];
+int t;
+int k;
+int fileSize[500][500];
+int dp[500][500];
 
-void Solve(int dp[][501], int k){
-    for(int i=k; i>0; i--){
-        for(int j=i; j<=k; j++){
-            if(i==j)    continue;
-            
-            int minCost = INF; int minSize = INF;
-            for(int p=0; p<j-i; p++){
-                int newFileSize = fileSize[i][i+p] + fileSize[i+p+1][j];
-                int temp = dp[i][i+p] + dp[i+p+1][j] + newFileSize;
-                if (temp <= minCost){
-                    minCost = temp;
-                    if(newFileSize < minSize)
-                        minSize = newFileSize;
-                }
-            }
-            dp[i][j] = minCost;
-            fileSize[i][j] = minSize;
-        }
-    }
-}
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
 
-int main(){
-    int t;
-    cin>>t;
-    for(int i=0; i<t; i++){
-        int k;
-        cin>>k;
-        int dp[501][501];
-        for(int j=1; j<k+1; j++){
-            int n;
-            cin>>n;
-            fileSize[j][j] = n;
-            dp[j][j] = 0;
-        }
-        Solve(dp,k);
-        cout<<dp[1][k]<<'\n';
-    }
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> k;
+		for (int i = 0; i < k; ++i)
+		{
+			cin >> fileSize[i][i];
+		}
+
+		for (int i = k - 1; i >= 0; --i)
+		{
+			for (int j = i; j < k; ++j)
+			{
+				if (i == j)	continue;
+				
+				//dp[i][i] + dp[i + 1][j];
+				//dp[i][i + 1] + dp[i + 2][j];
+				//// . . .
+				//dp[i][j - 2] + dp[j - 1][j];
+				//dp[i][j - 1] + dp[j][j];
+
+				int minCost = 500000000;
+				int size = 500000000;
+				for (int l = i; l < j; ++l)
+				{
+					int newFileSize = fileSize[i][l] + fileSize[l+1][j];
+					int cost = dp[i][l] + dp[l + 1][j] + newFileSize;
+					if (cost <= minCost)
+					{
+						minCost = cost;
+						size = min(size, newFileSize);
+					}
+				}
+				dp[i][j] = minCost;
+				fileSize[i][j] = size;
+			}
+		}
+		cout << dp[0][k - 1] << '\n';
+	}
 }
