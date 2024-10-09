165679a354a8fb6b0455e7d5deab753edc572204
Mingi Kang <rkdalsrl111@naver.com>
2024-10-09 20:27:12 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -2,35 +2,36 @@
 
 using namespace std;
 
-int n,m;
-int path[8];
-bool visited[9];
+int n, m;
+int path[10];
+bool visited[10];
 
 void Recur(int level)
 {
-    if(level == m)
-    {
-        for(int i : path)
-        {
-            if(!i)  break;
-            cout<<i<<' ';
-        }
-        cout<<'\n';
-        return;
-    }
-    for(int i=1; i<=n; i++)
-    {
-        if(visited[i])  continue;
-        visited[i] = true;
-        path[level] = i;
-        Recur(level+1);
-        visited[i] = false;
-    }
+	if (level == m)
+	{
+		for (int i = 0; i < level; ++i)
+		{
+			cout << path[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (visited[i])	continue;
+		visited[i] = true;
+		path[level] = i;
+		Recur(level + 1);
+		visited[i] = false;
+	}
 }
 
 int main()
 {
-    cin.tie(nullptr); ios::sync_with_stdio(false);
-    cin>>n>>m;
-    Recur(0);
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	cin >> n >> m;
+
+	Recur(0);
 }
