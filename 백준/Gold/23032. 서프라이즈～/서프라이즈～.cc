fdc898c04051c2a7b33de858dd5dfd63e6a97a93
Mingi Kang <rkdalsrl111@naver.com>
2024-10-14 16:45:01 +0900

    [Gold III] Title: 서프라이즈~, Time: 16 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/23032. 서프라이즈～/서프라이즈～.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[2020];
+int cumSum[2020];
+
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> arr[i];
+		cumSum[i] = cumSum[i - 1] + arr[i];
+	}
+
+	int minDiff = 2147483647;
+	int ans = 0;
+	for (int mid = 1; mid < n; ++mid)
+	{
+		int s = mid, e = mid + 1;
+		while (s > 0 && e <= n)
+		{
+			int leftSum = cumSum[mid] - cumSum[s - 1];
+			int rightSum = cumSum[e] - cumSum[mid];
+			int diff = abs(leftSum - rightSum);
+
+
+			if (diff < minDiff)
+			{
+				minDiff = diff;
+				ans = leftSum + rightSum;
+			}
+			else if (diff == minDiff)
+			{
+				ans = max(ans, leftSum + rightSum);
+			}
+
+			if (leftSum > rightSum)
+			{
+				++e;
+			}
+			else if (leftSum < rightSum)
+			{
+				--s;
+			}
+			else
+			{
+				--s;
+				++e;
+			}
+
+		}
+	}
+	cout << ans << endl;
+}
