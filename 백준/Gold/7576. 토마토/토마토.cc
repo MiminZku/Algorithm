d48ed27f3756e701ef3961b2cb96a64e3313d42c
Mingi Kang <rkdalsrl111@naver.com>
2024-11-02 02:03:56 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,56 +1,53 @@
-#include <cstdio>
+#include <iostream>
 #include <queue>
-#include <vector>
+
 using namespace std;
 
-typedef pair<int,int> Index;
-int m,n;
+typedef pair<int,int> Idx;
 
-Index operator + (const Index & p1, const Index & p2){
-    return {p1.first+p2.first, p1.second+p2.second};
+int mat[1000][1000];
+int n,m;
+int day;
+Idx navers[4] = {{0,-1}, {0,1}, {-1,0}, {1,0}};
+queue<pair<Idx,int>> q;
+
+void BFS(){
+    while(!q.empty()){
+        Idx cur = q.front().first;
+        int d = q.front().second;
+        day = d;
+        q.pop();
+
+        for(Idx naver : navers){
+            int ny = cur.first + naver.first;
+            int nx = cur.second + naver.second;
+            if(ny<0 || nx<0 || ny>=n || nx>=m)  continue;
+            if(mat[ny][nx]) continue;
+            mat[ny][nx] = 1;
+            q.push({{ny,nx},d+1});
+        }
+    }
+}
+
+bool Check(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(mat[i][j]==0)    return false;
+        }
+    }
+    return true;
 }
 
 int main(){
-    scanf("%d %d",&m, &n);
-    int mat[n][m];
-    vector<Index> start;
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    cin>>m>>n;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            scanf("%d",&mat[i][j]);
-            if(mat[i][j]==1)   start.push_back({i,j});
+            cin>>mat[i][j];
+            if(mat[i][j]==1)    q.push({{i,j},day});
         }
     }
-    int res = 0;
-    Index nav[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-    queue<pair<Index,int>> q;
-    for(Index i : start){
-        q.push({i,0});
-    }
-    while(!q.empty()){
-        Index cur = q.front().first;
-        int count = q.front().second;
-        q.pop();
-
-        vector<Index> navers;
-        for(Index idx : nav){
-            Index nav_idx = idx + cur;
-            if(nav_idx.first < 0 or nav_idx.second < 0
-                or nav_idx.first >= n or nav_idx.second >= m)   continue;
-            int nav_val = mat[nav_idx.first][nav_idx.second];
-            if(nav_val != 0) continue;
-            mat[nav_idx.first][nav_idx.second] = 1;
-            q.push({nav_idx,count+1});
-            res = count+1;
-        }
-    }
-    for (int i = 0; i<n ; i++){
-        for(int j=0; j<m; j++){
-            // printf("%2d ",mat[i][j]);
-            if(mat[i][j]==0){
-                res = -1;
-            }
-        }
-        // printf("\n");
-    }
-    printf("%d\n",res);
+    BFS();
+    if(Check()) cout<<day<<endl;
+    else        cout<<-1<<endl;
 }
