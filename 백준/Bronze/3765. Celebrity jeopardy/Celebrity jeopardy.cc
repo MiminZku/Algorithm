4d7f5f53ff0bbc3de480cb21993fb7aeaed15411
Mingi Kang <rkdalsrl111@naver.com>
2024-12-09 22:04:18 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+
+    string str;
+    while(getline(cin, str))
+    {
+        cout<<str<<endl;
+    }
+}
