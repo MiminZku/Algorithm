527cc85cfa2d69c9a2dc6b55188e78f92c40261b
Mingi Kang <rkdalsrl111@naver.com>
2025-03-25 02:07:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,55 +1,52 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-char org[51];
-vector<int> nums;
-vector<char> opers;
+int main()
+{
+	string str;
+	cin >> str;
+	str.append(" ");
 
-int main(){
-    cin>>org;
-    char temp[51] = {};
-    int idx = 0;
-    for(char& c: org){
-        if(c){
-            if(c=='+' || c=='-'){
-                opers.push_back(c);
-                int k = 1;
-                int n = 0;
-                for(int i=idx-1; i>=0; i--){
-                    n += (temp[i]-'0') * k;
-                    k *= 10;
-                }
-                idx = 0;
-                nums.push_back(n);
-            }
-            else{
-                temp[idx++] = c;
-            }
-        }
-        else{
-            int k = 1;
-            int n = 0;
-            for(int i=idx-1; i>=0; i--){
-                n += (temp[i]-'0') * k;
-                k *= 10;
-            }
-            idx = 0;
-            nums.push_back(n);
-            break;
-        }
-    }
-    for(int i=opers.size()-1; i>=0; i--){
-        if(opers[i]=='+'){
-            nums[i+1] = nums[i]+nums[i+1];
-            nums.erase(nums.begin()+i);
-            opers.erase(opers.begin()+i);
-        }
-    }
-    int result = nums[0];
-    for(int i=0; i<opers.size(); i++){
-        result -= nums[i+1];
-    }
-    cout<<result<<endl;
+	vector<int> numList;
+	vector<char> opList;
+	string temp = "";
+	for (char ch : str)
+	{
+		if (ch >= '0' && ch <= '9')
+		{
+			temp.append(1, ch);
+			continue;
+		}
+
+		if (ch == '+' || ch == '-')
+		{
+			opList.push_back(ch);
+		}
+	
+		int num = stoi(temp);
+		numList.push_back(num);
+		temp = "";
+	}
+
+	for (int i = 0; i < opList.size(); ++i)
+	{
+		if (opList[i] != '+')	continue;
+		
+		numList[i] += numList[i + 1];
+		numList.erase(numList.begin() + i + 1);
+
+		opList.erase(opList.begin() + i);
+		--i;
+	}
+
+	int ans = numList[0];
+	for (int i = 1; i < numList.size(); ++i)
+	{
+		ans -= numList[i];
+	}
+
+	cout << ans << endl;
 }
