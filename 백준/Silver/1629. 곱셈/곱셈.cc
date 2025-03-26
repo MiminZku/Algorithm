bd0a69ff5ad1463fe0d6aa609d2e87fd6ccdd70f
Mingi Kang <rkdalsrl111@naver.com>
2025-03-26 13:22:40 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -7,7 +7,6 @@ long long a,b,c;
 long long Mul(long long n, long long m)
 {
     if(m==0)    return 1;
-    if(m==1)    return a % c;
     
     long long ret = Mul(n, m/2);
     ret = (ret % c) * (ret % c) % c;
