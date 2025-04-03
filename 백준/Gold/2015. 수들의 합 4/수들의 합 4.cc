ce406617b571b9748880ea2ac0a060959e23c70c
Mingi Kang <rkdalsrl111@naver.com>
2025-04-03 13:44:59 +0900

    [Gold IV] Title: 수들의 합 4, Time: 28 ms, Memory: 7600 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 
 int n, k;
-vector<int> preSum;
-unordered_map<int, int> cntMap;
+int preSum[200002];
+unordered_map<int, int> sumCnt;
 long long ans;
 
 int main()
 {
 	cin.tie(nullptr); ios::sync_with_stdio(false);
 	cin >> n >> k;
-	preSum.resize(n + 1, 0);
 	for (int i = 1; i <= n; ++i)
 	{
-		int j;
-		cin >> j;
-		preSum[i] = j + preSum[i - 1];
+		cin >> preSum[i];
+		preSum[i] += preSum[i - 1];
 
 		if (preSum[i] == k)
+		{
 			++ans;
-		if (cntMap[preSum[i] - k] > 0)
-			ans += cntMap[preSum[i] - k];
-
-		++cntMap[preSum[i]];
+		}
+		if (sumCnt.find(preSum[i] - k) != sumCnt.end())
+		{
+			ans += sumCnt[preSum[i] - k];
+		}
+		++sumCnt[preSum[i]];
 	}
 	cout << ans << endl;
 }
