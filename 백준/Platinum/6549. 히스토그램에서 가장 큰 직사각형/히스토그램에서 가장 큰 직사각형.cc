83a52ccb9ff4e2a87ff44ba9512a37a926a5a339
Mingi Kang <rkdalsrl111@naver.com>
2025-04-05 03:54:27 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 3744 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+
+	while (true)
+	{
+		long long ans = 0;
+		stack<pair<int, long long>> st;
+		cin >> n;
+		if (n == 0)
+		{
+			break;
+		}
+		for (int i = 0, k; i < n + 1; ++i)
+		{
+			if (i == n)
+			{
+				k = 0;
+			}
+			else
+			{
+				cin >> k;
+			}
+
+			int l = i;
+			while (!st.empty() && k < st.top().second)
+			{
+				l = st.top().first;
+				ans = max(ans, (i - l) * st.top().second);
+				st.pop();
+			}
+			st.push({ l, k });
+		}
+
+		cout << ans << '\n';
+	}
+}
