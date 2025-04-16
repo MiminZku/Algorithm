57708f0e3428a75fe2b3e629b68a33a47616cd9a
Mingi Kang <rkdalsrl111@naver.com>
2025-04-16 13:38:18 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,50 +1,60 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int mat[25][25];
 int n;
-vector<int> pops;
-int cnt;
+int mat[25][25];
 
-void Color(int r, int c, int color){
-    mat[r][c] = color;
-    cnt++;
+int dy[] = { 0,0,-1,1 };
+int dx[] = { -1,1,0,0 };
 
-    int dy[4] = {0,0,-1,1};
-    int dx[4] = {1,-1,0,0,};
-    for(int i=0; i<4; i++){
-        int ny = r+dy[i];
-        int nx = c+dx[i];
-        if(ny<0 || ny>=n || nx<0 || nx>=n)  continue;
-        if(mat[ny][nx]!=-1) continue;
-        Color(ny,nx,color);
-    }
+int Paint(int y, int x, int color)
+{
+	mat[y][x] = color;
+	int ret = 1;
+	for (int i = 0; i < 4; ++i)
+	{
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n)	continue;
+		if (mat[ny][nx] != -1)	continue;
+		ret += Paint(ny, nx, color);
+	}
+	return ret;
 }
 
-int main(){
-    cin>>n;
-    for(int i=0; i<n; i++){
-        char str[26] = {};
-        cin>>str;
-        for(int j=0; j<n; j++){
-            int value = str[j]-'0';
-            if(value)   mat[i][j] = -1;
-            else        mat[i][j] = 0;
-        }        
-    }
-    int k=0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(mat[i][j] != -1)  continue;
-            Color(i,j,++k);
-            pops.push_back(cnt);
-            cnt = 0;
-        }
-    }
-    cout<<k<<endl;
-    sort(pops.begin(),pops.end());
-    for(int i : pops)   cout<<i<<endl;
+int main()
+{
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	cin >> n;
+	cin.ignore();
+	string str;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> str;
+		for (int j = 0; j < n; ++j)
+		{
+			mat[i][j] = (str[j] - '0') * -1;
+		}
+	}
+
+	vector<int> arr;
+	for (int i = 0, color = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if (mat[i][j] != -1)	continue;
+			arr.push_back(Paint(i, j, ++color));
+		}
+	}
+
+	sort(arr.begin(), arr.end());
+
+	cout << arr.size() << endl;
+	for (int i : arr)
+	{
+		cout << i << '\n';
+	}
 }
