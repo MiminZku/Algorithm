39352a1cd8c0decca04c216a3a3504623a43062f
Mingi Kang <rkdalsrl111@naver.com>
2025-04-19 23:07:27 +0900

    [Gold V] Title: 평범한 배낭, Time: 64 ms, Memory: 41864 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,39 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <memory.h>
 
 using namespace std;
 
 int n, k;
-pair<int, int> arr[100];
-int ans;
-int dp[100][100001];
+pair<int, int> arr[100000];
+int memo[100][100001];
 
-int Recur(int cur, int weight) // cur번째 까지 왔는데 무게가 weight 만큼 찼을 때 최대 가치 반환
+int Recur(int idx, int weight)  
 {
-	if(weight > k)	return -2147483647;
-	if (cur == n)	return 0;
+    if(weight > k)
+    {
+        return -2147483648;
+    }
+    if(idx == n)
+    {
+        return 0;
+    }
 
-	if (dp[cur][weight] != -1)	return dp[cur][weight];
+    if(memo[idx][weight] != -1)
+    {
+        return memo[idx][weight];
+    }
 
-	// cur번 째 물건을 선택할 경우
-	int temp = Recur(cur + 1, weight + arr[cur].first) + arr[cur].second;
-
-	// 선택 안할 경우
-	int temp2 = Recur(cur + 1, weight);
-
-	return dp[cur][weight] = max(temp, temp2);
+    return memo[idx][weight] = 
+        max(
+            Recur(idx + 1, weight),
+            Recur(idx + 1, weight + arr[idx].first) + arr[idx].second);
 }
 
 int main()
 {
-	cin.tie(nullptr); ios::sync_with_stdio(false);
-	cin >> n >> k;
-	memset(dp, -1, sizeof(dp));
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> arr[i].first >> arr[i].second;
-	}
-	
-	cout << Recur(0, 0) << endl;
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    memset(memo, -1, sizeof(memo));
+    cin >> n >> k;
+    for(int i=0; i<n; ++i)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+
+    cout << Recur(0, 0) << endl;
 }
