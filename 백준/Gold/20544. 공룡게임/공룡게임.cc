2298f5d3024cd470372fdef458702081495297c1
Mingi Kang <rkdalsrl111@naver.com>
2025-04-27 22:11:58 +0900

    [Gold IV] Title: 공룡게임, Time: 0 ms, Memory: 2092 KB -BaekjoonHub

--- 백준/Gold/20544. 공룡게임/공룡게임.cc
+++ 백준/Gold/20544. 공룡게임/공룡게임.cc
@@ -6,39 +6,39 @@
 using namespace std;
 
 int n;
-int memo[1000][3][1000][3];
+int memo[1000][3][3][2];
 
 // idx : 현재 좌표
 // state : 지점의 상태 (0 = 바닥, 1 or 2 = 선인장) 
-// num : 높이 2인 선인장 개수
 // con : 지금까지 연속한 선인장 개수
-int Recur(int idx, int state, int num, int con)
+// check : 높이 2인 선인장 있는지
+int Recur(int idx, int state, int con, bool check)
 {
 	if (idx == n)
 	{
-		if (num == 0)	return 0;
+		if (check == false)	return 0;
 		return 1;
 	}
 
-	if (memo[idx][state][num][con] != -1)
+	if (memo[idx][state][con][check] != -1)
 	{
-		return memo[idx][state][num][con];
+		return memo[idx][state][con][check];
 	}
 
 	if (con == 2)
 	{
-		return memo[idx][state][num][con] = Recur(idx + 1, 0, num, 0) % MOD;
+		return memo[idx][state][con][check] = Recur(idx + 1, 0, 0, check) % MOD;
 	}
 
 	int ret = 0;
 	for (int s = 0; s < 3; ++s)
 	{
-		if (s == 2 && state == 2)	break;
-		ret += Recur(idx + 1, s, num + (s == 2), (s == 0) ? 0 : con + 1) % MOD;
-	    ret %= MOD;
-    }
+		if (s == 2 && state == 2 && con == 1)	break;
+		ret += Recur(idx + 1, s, (s == 0) ? 0 : con + 1, check || (s == 2)) % MOD;
+		ret %= MOD;
+	}
 
-	return memo[idx][state][num][con] = ret;
+	return memo[idx][state][con][check] = ret % MOD;
 }
 
 int main()
