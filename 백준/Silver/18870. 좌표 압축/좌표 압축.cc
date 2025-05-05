f80f6a7ecfd64657586313548616020324f38bff
Mingi Kang <rkdalsrl111@naver.com>
2025-05-05 18:37:28 +0900

    [Silver II] Title: 좌표 압축, Time: 440 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,49 +1,33 @@
+#include <iostream>
 #include <algorithm>
-#include <cstdio>
 #include <vector>
+
 using namespace std;
 
 int n;
+vector<int> arr;
+vector<int> sortedArr;
 
-int BinarySearch(vector<int>& vec, int start, int end, int target){
-    if (start == end){
-        if(target==vec[start]){
-            return start;
-        }
-        return -1;
-    }
-    int mid = (start+end)/2;
-    int idx;
-    if (vec[mid] < target){
-        idx = BinarySearch(vec, mid+1, end, target);
-    }
-    else{
-        idx = BinarySearch(vec, start, mid, target);
-    }
-    return idx;
-}
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
 
-int main(){
-    scanf("%d",&n);
-    int arr[n]; int org[n];
-    for(int i = 0; i<n ; i++){
-        int x;
-        scanf("%d", &x);
-        arr[i] = x;
-        org[i] = x;
-    }
-    sort(arr, arr+n);
-    vector<int> de_duplicate_arr;
-    de_duplicate_arr.push_back(arr[0]);
-    for(int i=1; i<n; i++){
-        if(arr[i] != arr[i-1]){
-            de_duplicate_arr.push_back(arr[i]);
-        }
-    }
+	cin >> n;
+	arr.resize(n);
+	sortedArr.resize(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+		sortedArr[i] = arr[i];
+	}
 
-    for(int i: org){
-        printf("%d ",BinarySearch(de_duplicate_arr, 0, de_duplicate_arr.size()-1, i));
-    }
-    printf("\n");
-    return 0;
+	sort(sortedArr.begin(), sortedArr.end());
+	sortedArr.erase(unique(sortedArr.begin(), sortedArr.end()), sortedArr.end());
+
+	for (int i : arr)
+	{
+		cout << (lower_bound(sortedArr.begin(), sortedArr.end(), i) - sortedArr.begin()) << ' ';
+	}
+	cout << endl;	
 }
