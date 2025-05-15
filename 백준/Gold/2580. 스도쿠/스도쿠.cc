c78444830ce7f7cd374aa19fdcb5572d8a89ad55
Mingi Kang <rkdalsrl111@naver.com>
2025-05-16 02:09:15 +0900

    [Gold IV] Title: 스도쿠, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -6,11 +6,19 @@ bool usedRow[9][10];
 bool usedCol[9][10];
 bool usedBox[9][10];
 
-int board[81];
+int board[9][9];
 
 int BoxNum(int r, int c)
 {
 	return (r / 3) * 3 + (c / 3);
+}
+
+void CheckUsed(int r, int c, int num, bool used)
+{
+	usedRow[r][num] = used;
+	usedCol[c][num] = used;
+	usedBox[BoxNum(r, c)][num] = used;
+	board[r][c] = used ? num : 0;
 }
 
 void Recur(int r, int c)
@@ -26,14 +34,14 @@ void Recur(int r, int c)
 		{
 			for (int j = 0; j < 9; ++j)
 			{
-				cout << board[i * 9 + j] << ' ';
+				cout << board[i][j] << ' ';
 			}
 			cout << '\n';
 		}
 		exit(0);
 	}
 
-	if (0 != board[r * 9 + c])
+	if (0 != board[r][c])
 	{
 		Recur(r, c + 1);
 		return;
@@ -41,24 +49,13 @@ void Recur(int r, int c)
 
 	for (int i = 1; i <= 9; ++i)
 	{
-		if (usedRow[r][i] ||
-			usedCol[c][i] ||
-			usedBox[BoxNum(r, c)][i])
+		if (usedRow[r][i] || usedCol[c][i] || usedBox[BoxNum(r, c)][i])
 		{
 			continue;
 		}
-		usedRow[r][i] = true;
-		usedCol[c][i] = true;
-		usedBox[BoxNum(r, c)][i] = true;
-		board[r * 9 + c] = i;
-
-		
+		CheckUsed(r, c, i, true);
 		Recur(r, c + 1);
-
-		usedRow[r][i] = false;
-		usedCol[c][i] = false;
-		usedBox[BoxNum(r, c)][i] = false;
-		board[r * 9 + c] = 0;
+		CheckUsed(r, c, i, false);
 	}
 }
 
@@ -70,11 +67,7 @@ int main()
 		{
 			int k;
 			cin >> k;
-			board[i * 9 + j] = k;
-
-			usedRow[i][k] = true;
-			usedCol[j][k] = true;
-			usedBox[BoxNum(i, j)][k] = true;
+			CheckUsed(i, j, k, true);
 		}
 	}
 
