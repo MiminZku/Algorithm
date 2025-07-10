eaf330cc4af34ea0e331f634d7db6eb12134c19c
Mingi Kang <rkdalsrl111@naver.com>
2025-07-10 17:07:56 +0900

    [Gold II] Title: Irrational Division, Time: 4 ms, Memory: 2120 KB -BaekjoonHub

---
+++ 백준/Gold/15011. Irrational Division/Irrational Division.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int p, q;
+bool visited[100][100][2];
+int memo[100][100][2];
+
+int Recur(int rowLen, int colLen, bool isBlack)
+{
+	if (rowLen == 0 || colLen == 0)
+	{
+		return 0;
+	}
+	if (rowLen == 1 && colLen == 1)
+	{
+		return isBlack ? 1 : -1;
+	}
+
+	if (visited[rowLen][colLen][isBlack])
+	{
+		return memo[rowLen][colLen][isBlack];
+	}
+
+	int ret = -10000;
+	for (int i = 1; i <= colLen; ++i)
+	{
+		int tmp = 0;
+		if (i % 2 == 1 && rowLen % 2 == 1)
+		{
+			if (isBlack)
+			{
+				tmp += 1;
+			}
+			else
+			{
+				tmp -= 1;
+			}
+		}
+		ret = max(ret, tmp - Recur(colLen - i, rowLen, (i % 2) ? !isBlack : isBlack));
+	}
+
+	visited[rowLen][colLen][isBlack] = true;
+	return memo[rowLen][colLen][isBlack] = ret;
+}
+
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	cin >> p >> q;
+
+	cout << Recur(p, q, true) << endl;
+}
