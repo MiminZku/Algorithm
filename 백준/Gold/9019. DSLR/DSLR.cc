3a9cb7ebc358bf2f9293f8e5f1280c53f16e93cd
Mingi Kang <rkdalsrl111@naver.com>
2025-09-01 14:00:37 +0900

    [Gold IV] Title: DSLR, Time: 1576 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,77 +1,90 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
-
-#define D 0
-#define S 1
-#define L 2
-#define R 3
 
 using namespace std;
 
-int t;
-bool visited[10000];
-int parent[10000];
-int command[10000];
+char i2c[4] = { 'D', 'S', 'L', 'R' };
 
-int DSLR(int command, int num){
-    int result;
-    if(command == D)    result = (num*2)%10000;
-    else if(command == S){
-        result = num-1;
-        if(result<0)   result = 9999;
-    }
-    else if(command == L)   result = (num%1000)*10 + num/1000;
-    else if(command == R)   result = (num%10)*1000 + num/10;
-    return result;
+int a, b;
+
+int DSLR(int cmd, int num) 
+{
+	if (i2c[cmd] == 'D')
+	{
+		return (num * 2) % 10000;
+	}
+	else if (i2c[cmd] == 'S')
+	{
+		if (num == 0)
+		{
+			return 9999;
+		}
+		return num - 1;
+	}
+	else if (i2c[cmd] == 'L')
+	{
+		return (num % 1000) * 10 + num / 1000;
+	}
+	else if (i2c[cmd] == 'R')
+	{
+		return (num % 10) * 1000 + num / 10;
+	}
 }
 
-void Solve(int a, int b, vector<int>& result){
-    fill_n(parent,10000,-1);
-    fill_n(command,10000,-1);
-    fill_n(visited,10000,false);
-    queue<pair<int,pair<int,int>>> q;
-    q.push({a,{-1,-1}});
-    visited[a] = true;
+void BFS(vector<char>& ans)
+{
+	vector<bool> visited(10000, false);
+	vector<int> parent(10000, -1);
+	vector<char> cmd(10000, -1);
 
-    while(!q.empty()){
-        int cur = q.front().first;
-        q.pop();
+	queue<int> q;
+	q.push(a);
+	visited[a] = true;
 
-        if(cur==b){
-            while(parent[cur]>=0){
-                result.push_back(command[cur]);
-                cur = parent[cur];
-            }
-            reverse(result.begin(),result.end());
-            return;
-        }
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
 
-        for(int i=0; i<4; i++){
-            int next = DSLR(i, cur);
-            if(visited[next])   continue;
-            visited[next] = true;
-            parent[next] = cur;
-            command[next] = i;
-            q.push({next,{cur,i}});
-        }
-    }
+		if (cur == b)
+		{
+			while (parent[cur] != -1)
+			{
+				ans.push_back(cmd[cur]);
+				cur = parent[cur];
+			}
+			reverse(ans.begin(), ans.end());
+			return;
+		}
+
+		for (int i = 0; i < 4; ++i)
+		{
+			int next = DSLR(i, cur);
+			if (visited[next]) continue;
+			visited[next] = true;
+			parent[next] = cur;
+			cmd[next] = i2c[i];
+			q.push(next);
+		}
+	}
 }
 
-int main(){
-    cin.tie(nullptr); ios::sync_with_stdio(false);
-    cin>>t;
-    while(t--){
-        int a, b;
-        cin>>a>>b;
-        vector<int> result;
-        Solve(a, b, result);
-        for(int i: result){
-            if(i==D)        cout<<'D';
-            else if(i==S)   cout<<'S';
-            else if(i==L)   cout<<'L';
-            else if(i==R)   cout<<'R';
-        }
-        cout<<'\n';
-    }
+int main()
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		cin >> a >> b;
+		vector<char> ans;
+		BFS(ans);
+		for (char ch : ans)
+		{
+			cout << ch;
+		}
+		cout << '\n';
+	}
 }
